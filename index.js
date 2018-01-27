0bec58dfca4e1f08d8d27b1c87674bcf172a9ca0
Christian Weiss <worldmover777@gmail.com>
2018-01-27 20:31:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function add(a,b){
   return a+b
 }
-function sub(a,b){
+function sutract(a,b){
   return a-b
 }
+function multiply(a,b){
+  return a*b
+}
+function divide(a,b){
+  return a/b
+}
+function 
